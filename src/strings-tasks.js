6d5854d928cc85a87c50f5166a2ca7aad8c7b0c8
Evgeny Kuptsov <kupzov200@mail.ru>
2023-11-22 17:47:58 +0400

    feat: complete "Returns the number of vowels in the string" task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -301,8 +301,11 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  return str
+    .split('')
+    .filter((elem) => /[aeiouyAEIOUY]/.test(elem))
+    .join('').length;
 }
 
 /**
