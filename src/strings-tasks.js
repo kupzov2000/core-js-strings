a98c430dc1c19192d6db92624db5aef56b822d7c
Evgeny Kuptsov <kupzov200@mail.ru>
2025-10-25 11:52:30 +0400

    feat: perform the countVowels task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -309,8 +309,14 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  return str
+    .split('')
+    .filter((item) =>
+      ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'].includes(
+        item
+      )
+    ).length;
 }
 
 /**
