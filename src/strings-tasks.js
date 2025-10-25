3c98c6f47ec198d25015144f9fb790f155d6b0d0
Evgeny Kuptsov <kupzov200@mail.ru>
2025-10-25 09:59:33 +0400

    feat: perform the getStringLength and isString tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -20,7 +20,7 @@
  *   getStringLength(undefined) => 0
  */
 function getStringLength(value) {
-  throw new Error('Not implemented');
+  return value ? value.length : 0;
 }
 
 /**
@@ -37,8 +37,8 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 /**
