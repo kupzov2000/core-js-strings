34f5ad483ab4856f87e1d8f919789c8d03af6b58
Evgeny Kuptsov <kupzov200@mail.ru>
2025-10-25 12:16:12 +0400

    feat: perform the invertCase task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -392,8 +392,13 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((item) =>
+      item.toUpperCase() === item ? item.toLowerCase() : item.toUpperCase()
+    )
+    .join('');
 }
 
 /**
